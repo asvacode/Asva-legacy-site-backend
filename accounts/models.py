df4c7ccd146b0d4e8dad092c1c85fe45b67386f2
Amaryllis750 <danielchukwuemela751@gmail.com>
2026-09-20 18:48:19 +0100

    feat: included phone number in sign up process

--- accounts/models.py
+++ accounts/models.py
@@ -2,11 +2,12 @@ from __future__ import annotations
 
 from django.conf import settings
 from django.db import models
-
+from phonenumber_field.modelfields import PhoneNumberField
 
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile")
     reference_code = models.CharField(max_length=32, unique=True, db_index=True)
+    phone_number = PhoneNumberField(blank=True, region="NG")
 
     def __str__(self) -> str:
         return f"{self.user.username} ({self.reference_code})"
